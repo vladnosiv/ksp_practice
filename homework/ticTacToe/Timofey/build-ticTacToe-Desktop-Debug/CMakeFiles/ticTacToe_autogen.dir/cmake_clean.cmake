cc2f445fcfa0cc01d0f6decc3d970602379fda11
MaxanRus <timosha1113@yandex.ru>
2020-05-02 13:10:19 +0300

    Init commit

--- homework/ticTacToe/Timofey/build-ticTacToe-Desktop-Debug/CMakeFiles/ticTacToe_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ticTacToe_autogen"
-  "ticTacToe_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ticTacToe_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
